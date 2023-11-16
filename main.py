9a3a0b28a9a6ff0a2c982b2fc233ad87ed932140
Ducky3264 <david.love.wii@gmail.com>
2023-11-16 14:15:54 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import turtle
+
+if __name__ == "__main__":
+    print("Hello world!")
+    t = turtle.Turtle()
+    screen = turtle.Screen()
+    t.forward(90)
+    t.right(90)
+    t.forward(90)
+    turtle.mainloop()
