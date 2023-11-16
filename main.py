476743ac06dba94d8cfe09c5d57bb9d60b832bd0
Ducky3264 <david.love.wii@gmail.com>
2023-11-16 14:57:09 -0600

    menus better

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import turtle
 screen = turtle.Screen()
-
-def create_prompt(message_text, box_size, color, position: list):
+all_turtles = []
+def create_prompt(message_text, box_size_x, box_size_y, color, position: list):
     t = turtle.Turtle()
     # Start left corner
     t.penup()
@@ -9,24 +9,25 @@ def create_prompt(message_text, box_size, color, position: list):
     t.pendown()
     t.setheading(0)
     for _ in range(2):
-        t.forward(box_size)
+        t.forward(box_size_y)
         t.right(90)
-        t.forward(box_size / 2)
+        t.forward(box_size_x)
         t.right(90)
-    
+    all_turtles.append(t)
     return 
 
-def create_menu(size, num_boxes, list_messages, position: list):
+def create_menu(size_x, size_y, num_boxes, list_messages, position: list):
     for i in range(num_boxes):
-        position = [position[0] + i*((size + 50) / 2), position[1]]
-        create_prompt(list_messages[i], size / num_boxes, "red", position)
+        position = [position[0] + i*size_x, position[1]]
+        create_prompt(list_messages[i], size_x / num_boxes, size_y, "red", position)
 
 if __name__ == "__main__":
     screen.setup(width=800, height=600)
+    screen.screensize(800, 600)
     screen_size = [800, 600]
     print("Hello world!")
     options = ["Go to class now", "Sleep in"]
     count_msg = len(options)
-    create_menu(500, count_msg, options, [-350, -0])
+    create_menu(400, 300, count_msg, options, [-350, -0])
 
 turtle.mainloop()
