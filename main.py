cf6c8a02c51022ec00d8f9c2de672f77c313695d
Ducky3264 <david.love.wii@gmail.com>
2023-11-16 14:26:20 -0600

    message

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import turtle
+screen = turtle.Screen()
+
 
 if __name__ == "__main__":
     print("Hello world!")
-    t = turtle.Turtle()
-    screen = turtle.Screen()
-    t.forward(90)
-    t.right(90)
-    t.forward(90)
-    turtle.mainloop()
+    
